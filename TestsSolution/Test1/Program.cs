019879ac6dee434424c5555321c55d65b52cb846
Mamadou BANE <mabane@local.isima.fr>
2018-12-10 17:02:29 +0100

    TestsSolution

---
+++ TestsSolution/Test1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Principal;
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour {0} !", WindowsIdentity.GetCurrent().Name);
+        }
+    }
+}
